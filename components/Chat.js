7bf1f3ec12a1501dd7afdadf1e51148abcb9c40c
HIPgirl95 <hannah@dawne.com>
2025-01-07 09:32:13 -0500

    Start creating chat function

--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
+import { Avatar, GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
+  const [messages, setMessages] = useState([]);
   const { name, backgroundColor } = route.params;
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello Developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
+  }, []);
 
   useEffect(() => {
     navigation.setOptions({ title: name });
