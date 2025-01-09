c76d81e9ce4a09a4f50ff4329bc7b9b9eb3c733b
HIPgirl95 <hannah@dawne.com>
2025-01-09 16:05:01 -0500

    install and configure firebase

--- App.js
+++ App.js
@@ -4,15 +4,34 @@ import Start from "./components/Start";
 import Chat from "./components/Chat";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCgXr0moXGJ25N4elQNZ9U0rgN098fWNyk",
+    authDomain: "chat-app-6d139.firebaseapp.com",
+    projectId: "chat-app-6d139",
+    storageBucket: "chat-app-6d139.firebasestorage.app",
+    messagingSenderId: "40593220063",
+    appId: "1:40593220063:web:d40bfa8b4f8c5cccd3246a",
+  };
+
+  //initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  //initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Start">
         <Stack.Screen name="Start" component={Start} />
-        <Stack.Screen name="Chat" component={Chat} />
+        <Stack.Screen name="Chat">
+          {(props) => <Chat db={db} {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
