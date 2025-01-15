e846abd898f702bb29bc887cb53f22f7e5bf1f59
HIPgirl95 <hannah@dawne.com>
2025-01-15 15:15:33 -0500

    accessability

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -92,7 +92,14 @@ const CustomActions = ({
     }
   };
   return (
-    <TouchableOpacity style={styles.container} onPress={onActionPress}>
+    <TouchableOpacity
+      accessible={true}
+      accessibilityLabel="Attachments"
+      accessibilityHint="Lets you choose to send an image or your geolocation"
+      accessibilityRole="button"
+      style={styles.container}
+      onPress={onActionPress}
+    >
       <View style={[styles.wrapper, wrapperStyle]}>
         <Text style={[styles.iconText, iconTextStyle]}>+</Text>
       </View>
